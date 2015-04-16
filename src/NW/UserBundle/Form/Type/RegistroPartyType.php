20375c57c8fed495e87ba2f2578f052b920f1398
Sergio Enrique Vargas <sergioenrique@me.com>
2015-04-15 23:35:46 -0500

    Correccion de errores:

--- src/NW/UserBundle/Form/Type/RegistroPartyType.php
+++ src/NW/UserBundle/Form/Type/RegistroPartyType.php
@@ -18,25 +18,18 @@ class RegistroPartyType extends AbstractType
         $builder->add('apellidos', 'text');
         $builder->add('moneda', 'choice', array(
                 'choices' => array(
-                    'ARS' => 'ARS - Peso Argentino',
                     'AUD' => 'AUD - Australian Dollar',
-                    'BOB' => 'BOB - Boliviano',
                     'BRL' => 'BRL - Real Brasileiro',
                     'CAD' => 'CAD - Canadian Dollar',
-                    'CLP' => 'CLP - Peso Chileno',
-                    'COP' => 'COP - Peso Colombiano',
                     'DKK' => 'DKK - Danish Krone',
                     'EUR' => 'EUR - Euro',
                     'GBP' => 'GBP - Great Britain Pound',
-                    'GTQ' => 'GTQ - Quetzal Guatemalteco',
                     'HKD' => 'HKD - Hong Kong Dollar',
                     'MXN' => 'MXN - Peso Mexicano',
                     'NOK' => 'NOK - Norwegian Krone',
                     'NZD' => 'NZD - Nueva Zelanda Dollar',
-                    'PEN' => 'PEN - Sol Peruano',
                     'SEK' => 'SEK - Swedish Krone',
                     'USD' => 'USD - US Dollar',
-                    'VEF' => 'VEF - Bolivar Venezolano',
                 ),
                 'multiple'  => false,));
         $builder->add('userPass', 'password', array('mapped' => false, 'required'  => true));
