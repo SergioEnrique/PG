c109fd268f6f1b0820b22324513c8aa2abf1407d
Sergio Enrique Vargas <sergioenrique@me.com>
2015-05-14 10:57:47 -0500

    Muestra de errores y validacion de nuevo bucketgift

--- src/PG/PartyBundle/Form/BucketGiftType.php
+++ src/PG/PartyBundle/Form/BucketGiftType.php
@@ -5,6 +5,7 @@ namespace PG\PartyBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class BucketGiftType extends AbstractType
 {
@@ -15,8 +16,8 @@ class BucketGiftType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titulo', 'text', array('label' => 'Evento'))
-            ->add('date', 'text', array('label' => 'Fecha', 'mapped' => false))
+            ->add('titulo', 'text', array('label' => 'Evento', 'constraints' => new NotBlank()))
+            ->add('date', 'text', array('label' => 'Fecha', 'mapped' => false, 'constraints' => new NotBlank()))
         ;
     }
     
