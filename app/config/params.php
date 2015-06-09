cdb0a7f0e91175717bc02cf48b71fe7990f146ae
Sergio Enrique Vargas <sergioenrique@me.com>
2015-06-09 01:13:21 -0500

    configuraciones para windows y pdf con formato bonito de partygift y con traduccion

--- app/config/params.php
+++ app/config/params.php
@@ -1,7 +1,7 @@
 <?php
 # app/config/params.php
 
-// Configuraciones diferentes para version local y openshift
+// Si es Openshift
 if (getEnv("OPENSHIFT_APP_NAME")!='') {
 	$container->setParameter('database_host', getEnv("OPENSHIFT_MYSQL_DB_HOST"));
 	$container->setParameter('database_port', getEnv("OPENSHIFT_MYSQL_DB_PORT"));
@@ -18,19 +18,30 @@ if (getEnv("OPENSHIFT_APP_NAME")!='') {
 	    ),
 	));
 }
-else{
-	// Binario para crear pdfs es diferente en local que en servidor
-	/*$container->loadFromExtension('knp_snappy', array(
-	    'pdf' => array(
-	    	'enabled' => true,
-	    	'binary' => '/usr/local/bin/wkhtmltopdf',
-	    	'options' => array(),
-	    ),
-	));*/
+// Si es windows
+else if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+	// Base de datos
+	$container->setParameter('database_host', '127.0.0.1');
+	$container->setParameter('database_port', 'null');
+	$container->setParameter('database_name', 'partygift');
+	$container->setParameter('database_user', 'root');
+	$container->setParameter('database_password', 'newly$dbcon');
+
+	// PDF
 	$container->loadFromExtension('knp_snappy', array(
 	    'pdf' => array(
 	    	'enabled' => true,
 	    	'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
+	    	'options' => array(),
+	    ),
+	));
+}
+// Si es Unix
+else{
+	$container->loadFromExtension('knp_snappy', array(
+	    'pdf' => array(
+	    	'enabled' => true,
+	    	'binary' => '/usr/local/bin/wkhtmltopdf',
 	    	'options' => array(),
 	    ),
 	));
